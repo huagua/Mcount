0e8ccc8fb7cd6775d01240db8c8c71982a755eb1
hua <huagua>
2019-12-05 17:46:26 +0800

    简单美化了一下页面

--- app/src/main/java/com/example/mcount/MainActivity.java
+++ app/src/main/java/com/example/mcount/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.mcount;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -8,7 +10,6 @@ import android.view.View;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
@@ -18,9 +19,16 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        if(Build.VERSION.SDK_INT>=21){  //版本号判断（以下功能只在21及以上版本实现）
+            View decorView = getWindow().getDecorView();
+            decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN|View.SYSTEM_UI_FLAG_LAYOUT_STABLE);   //活动的布局会显示在状态栏上面
+            getWindow().setStatusBarColor(Color.TRANSPARENT);   //将状态栏设置为透明色
+        }
+
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        //Toolbar toolbar = findViewById(R.id.toolbar);
+        //setSupportActionBar(toolbar);
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
--- app/src/main/java/com/example/mcount/WriteDown.java
+++ app/src/main/java/com/example/mcount/WriteDown.java
@@ -16,19 +16,40 @@ public class WriteDown extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        /*
+        if(Build.VERSION.SDK_INT>=21){  //版本号判断（以下功能只在21及以上版本实现）
+            View decorView = getWindow().getDecorView();
+            decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN|View.SYSTEM_UI_FLAG_LAYOUT_STABLE);   //活动的布局会显示在状态栏上面
+            getWindow().setStatusBarColor(Color.TRANSPARENT);   //将状态栏设置为透明色
+        }
+
+         */
+
         setContentView(R.layout.activity_write_down);
         SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
         ViewPager viewPager = findViewById(R.id.view_pager);
         viewPager.setAdapter(sectionsPagerAdapter);
         TabLayout tabs = findViewById(R.id.tabs);
         tabs.setupWithViewPager(viewPager);
-        FloatingActionButton fab = findViewById(R.id.fab);
+        FloatingActionButton fabConfirm = findViewById(R.id.fab_confirm);
+        FloatingActionButton fabConcel = findViewById(R.id.fab_concel);
 
-        fab.setOnClickListener(new View.OnClickListener() {
+        fabConfirm.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent data = new Intent();
                 data.putExtra("data", "hellothefirstactivity");
+                setResult(2, data);
+                finish();
+            }
+        });
+
+        fabConcel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent data = new Intent();
+                data.putExtra("data", "aniyoaniyo");
                 setResult(2, data);
                 finish();
             }
