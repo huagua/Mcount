03f7baf6e41604736418955ef1bc8c3702102455
hua <huagua>
2019-12-15 00:24:06 +0800

    将时间与日期封装到dialog中

--- app/src/main/java/com/example/mcount/ui/main/PlaceholderFragment.java
+++ app/src/main/java/com/example/mcount/ui/main/PlaceholderFragment.java
@@ -1,5 +1,8 @@
 package com.example.mcount.ui.main;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -31,6 +34,8 @@ public class PlaceholderFragment extends Fragment {
     private PageViewModel pageViewModel;
 
     private DataBaseHelper mDataBase;
+
+    private String tmpDate = "";
 
     public static PlaceholderFragment newInstance(int index) {
         PlaceholderFragment fragment = new PlaceholderFragment();
@@ -66,11 +71,80 @@ public class PlaceholderFragment extends Fragment {
 
         final EditText inputMoney = root.findViewById(R.id.money);
         final EditText inputType = root.findViewById(R.id.type);
-        final TimePicker timePicker = root.findViewById(R.id.pick_time);
-        final DatePicker datePicker = root.findViewById(R.id.pick_date);
+
+        final TextView dateAndTime = root.findViewById(R.id.text_date_time);
 
         //获取当前tab的位置，1是支出，2是收入
         final int position = getArguments().getInt(ARG_SECTION_NUMBER);
+
+        //设置监听事件
+        dateAndTime.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View arg0) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+                View view = View.inflate(getActivity(), R.layout.dialog_date_time, null);
+                final TimePicker timePicker = view.findViewById(R.id.pick_time);
+                final DatePicker datePicker = view.findViewById(R.id.pick_date);
+                builder.setView(view);
+
+                builder.setTitle("选取起始时间");
+                builder.setPositiveButton("确  定", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                        if(Build.VERSION.SDK_INT>=21){
+
+                            String minute = "";
+                            if(timePicker.getMinute() < 10){
+                                minute+="0"+timePicker.getMinute();
+                            }else{
+                                minute+=timePicker.getMinute();
+                            }
+
+                            String date = "";
+                            if(datePicker.getDayOfMonth() < 10){
+                                date += " "+datePicker.getDayOfMonth();
+                            }else{
+                                date += datePicker.getDayOfMonth();
+                            }
+
+                            String month = "";
+                            if(datePicker.getMonth()+1 < 10){
+                                month += " "+(datePicker.getMonth()+1);
+                            }else{
+                                month += (datePicker.getMonth()+1);
+                            }
+
+                            String hour = "";
+                            if(timePicker.getHour() < 10){
+                                hour += " "+timePicker.getHour();
+                            }else{
+                                hour += timePicker.getHour();
+                            }
+
+                            tmpDate+=month+"-"+date+"  "+hour+":"+minute;
+                        }
+
+                        dateAndTime.setText(tmpDate);
+
+                        dialog.cancel();
+                    }
+                });
+
+                builder.setNegativeButton("取  消",new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+
+                Dialog dialog = builder.create();
+                dialog.show();
+            }
+        });
+
+
+
 
         fabConfirm.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -91,6 +165,7 @@ public class PlaceholderFragment extends Fragment {
                 //if(!inputType.getText().equals(""))
                     dailyCost.setName(inputType.getText().toString());
 
+                /*
                 String tmpDate = "";
                 if(Build.VERSION.SDK_INT>=21){
 
@@ -124,6 +199,11 @@ public class PlaceholderFragment extends Fragment {
 
                     tmpDate+=month+"-"+date+"  "+hour+":"+minute;
                 }
+
+                 */
+
+
+                tmpDate = dateAndTime.getText().toString();
 
                 if(!dailyCost.getCost().equals("") && !dailyCost.getCost().equals("")){
                     dailyCost.setDate(tmpDate);
