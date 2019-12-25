9cb2ec7ac083fe3657b4d9966e226f2815c25782
hua <1901210745@pku.edu.cn>
2019-12-25 22:20:18 +0800

    优化侧滑菜单栏

--- app/src/main/java/com/example/mcount/MainActivity.java
+++ app/src/main/java/com/example/mcount/MainActivity.java
@@ -1,7 +1,6 @@
 package com.example.mcount;
 
 import android.app.AlertDialog;
-import android.content.ContentValues;
 import android.content.Intent;
 import android.database.Cursor;
 import android.graphics.Bitmap;
@@ -10,7 +9,6 @@ import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
-import android.os.StrictMode;
 import android.provider.MediaStore;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -35,7 +33,6 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
-
 
 import static android.app.Activity.RESULT_OK;
 
