46c4ed3d161f33f5d4452f9ca0ceab6cae9fbb9e
hua <1901210745@pku.edu.cn>
2019-12-25 21:32:47 +0800

    解决一个bug，并且尝试用户更改头像，尚未成功……，代码先保留

--- app/src/main/java/com/example/mcount/MainActivity.java
+++ app/src/main/java/com/example/mcount/MainActivity.java
@@ -1,10 +1,17 @@
 package com.example.mcount;
 
+import android.app.AlertDialog;
+import android.content.ContentValues;
 import android.content.Intent;
 import android.database.Cursor;
+import android.graphics.Bitmap;
 import android.graphics.Color;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.os.StrictMode;
+import android.provider.MediaStore;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -22,8 +29,16 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.navigation.NavigationView;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
+
+
+import static android.app.Activity.RESULT_OK;
+
 
 /*
 * 主页面：显示账单列表
@@ -53,6 +68,9 @@ public class MainActivity extends AppCompatActivity {
     private Double totalInAccount = 0.0;
     private Double totalOutAccount = 0.0;
 
+    private Bitmap head;// 头像Bitmap
+    private static String path = "/sdcard/myHead/";// sd路径
+    private Uri imageUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -102,8 +120,7 @@ public class MainActivity extends AppCompatActivity {
         userHead.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
-                Intent intent = new Intent(MainActivity.this, Login.class);
-                startActivityForResult(intent, 2);
+                showTypeDialog();
             }
         });
 
@@ -115,6 +132,16 @@ public class MainActivity extends AppCompatActivity {
         navMenu.findItem(R.id.nav_register).setOnMenuItemClickListener(mListener);
         navMenu.findItem(R.id.nav_logout).setOnMenuItemClickListener(mListener);
 
+
+        /*
+        想解决打开相机闪退问题，但是没能解决
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            StrictMode.VmPolicy.Builder builder = new StrictMode.VmPolicy.Builder();
+            StrictMode.setVmPolicy(builder.build());
+        }
+
+         */
+
     }
 
     //点击菜单中的不同item的不同的响应事件
@@ -124,7 +151,7 @@ public class MainActivity extends AppCompatActivity {
             switch (item.getItemId()){
                 case R.id.nav_login:
                     Intent intent = new Intent(MainActivity.this, Login.class);
-                    startActivityForResult(intent, 2);
+                    startActivity(intent);
                     return true;
 
                 case R.id.nav_register:
@@ -171,8 +198,6 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onSwiped(RecyclerView.ViewHolder viewHolder, int direction) {
                 //侧滑事件
-                mDataBaseHelper.deleteCost(data.get(viewHolder.getAdapterPosition()));
-                data.remove(viewHolder.getAdapterPosition());
 
                 //删除之后重新设置总花销金额
                 totalAccount -= Double.parseDouble(data.get(viewHolder.getAdapterPosition()).getCost());
@@ -206,8 +231,8 @@ public class MainActivity extends AppCompatActivity {
                         totalIn.setText(totalInString);
                 }
 
-
-
+                mDataBaseHelper.deleteCost(data.get(viewHolder.getAdapterPosition()));
+                data.remove(viewHolder.getAdapterPosition());
                 mAdapter.notifyItemRemoved(viewHolder.getAdapterPosition());
             }
 
@@ -304,10 +329,106 @@ public class MainActivity extends AppCompatActivity {
 
     //从编辑页面传送信息到主页面，如果resultCode为2就刷新列表
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data){
-        super.onActivityResult(requestCode, resultCode, data);
+    protected void onActivityResult(int requestCode, int resultCode, Intent intentdata){
+        super.onActivityResult(requestCode, resultCode, intentdata);
         if(requestCode == 1 && resultCode == 2){
             updateAfterAddOne();
+        }else if(requestCode == 2 && resultCode == RESULT_OK){
+            File temp = new File(Environment.getExternalStorageDirectory() + "/head.jpg");
+            cropPhoto(Uri.fromFile(temp));// 裁剪图片
+        }else if(requestCode == 3){
+            if (intentdata != null) {
+                Bundle extras = intentdata.getExtras();
+                head = extras.getParcelable("data");
+                if (head != null) {
+                    /**
+                     * 上传服务器代码
+                     */
+                    setPicToView(head);// 保存在SD卡中
+                    userHead.setImageBitmap(head);// 用ImageButton显示出来
+                }
+            }
+        }else if(requestCode == 4 && resultCode == RESULT_OK){
+            cropPhoto(intentdata.getData());// 裁剪图片
+        }
+    }
+
+    public  void showTypeDialog() {
+        //显示对话框
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        final AlertDialog dialog = builder.create();
+        View view = View.inflate(this, R.layout.dialog_select_photo, null);
+        TextView tv_select_gallery = view.findViewById(R.id.tv_select_gallery);
+        TextView tv_select_camera =  view.findViewById(R.id.tv_select_camera);
+
+        tv_select_gallery.setOnClickListener(new View.OnClickListener() {// 在相册中选取
+            @Override
+            public void onClick(View v) {
+                Intent intent1 = new Intent(Intent.ACTION_PICK, null);
+                //打开文件
+                intent1.setDataAndType(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, "image/*");
+                startActivityForResult(intent1, 4);
+                dialog.dismiss();
+            }
+        });
+
+        tv_select_camera.setOnClickListener(new View.OnClickListener() {// 调用照相机
+            @Override
+            public void onClick(View v) {
+                Intent intent2 = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                intent2.putExtra(MediaStore.EXTRA_OUTPUT,
+                        Uri.fromFile(new File(Environment.getExternalStorageDirectory(), "head.jpg")));
+                startActivityForResult(intent2, 2);// 采用ForResult打开
+                dialog.dismiss();
+            }
+        });
+        dialog.setView(view);
+        dialog.show();
+    }
+
+    /**
+     * 调用系统的裁剪功能
+     *
+     * @param uri
+     */
+
+    public void cropPhoto(Uri uri) {
+        Intent intent = new Intent("com.android.camera.action.CROP");
+        intent.setDataAndType(uri, "image/*");
+        intent.putExtra("crop", "true");
+        // aspectX aspectY 是宽高的比例
+        intent.putExtra("aspectX", 1);
+        intent.putExtra("aspectY", 1);
+        // outputX outputY 是裁剪图片宽高
+        intent.putExtra("outputX", 150);
+        intent.putExtra("outputY", 150);
+        intent.putExtra("return-data", true);
+        startActivityForResult(intent, 3);
+
+    }
+
+    private void setPicToView(Bitmap mBitmap) {
+        String sdStatus = Environment.getExternalStorageState();
+        if (!sdStatus.equals(Environment.MEDIA_MOUNTED)) { // 检测sd是否可用
+            return;
+        }
+        FileOutputStream b = null;
+        File file = new File(path);
+        file.mkdirs();// 创建文件夹
+        String fileName = path + "head.jpg";// 图片名字
+        try {
+            b = new FileOutputStream(fileName);
+            mBitmap.compress(Bitmap.CompressFormat.JPEG, 100, b);// 把数据写入文件
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                // 关闭流
+                b.flush();
+                b.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 
